9d631ab074b33f384c4eb1f273101a94426c9c42
bradlet <bradlet2@pdx.edu>
2022-01-23 19:26:23 -0800

    Add planned functionality for main loop method

--- personal_work_dir/pycharm_project/1.4/hw1/hw1.py
+++ personal_work_dir/pycharm_project/1.4/hw1/hw1.py
@@ -50,6 +50,27 @@ login_url = f'https://{site}/login'
 resp = s.get(login_url)
 csrf = parse_tree_for_csrf(resp.text)
 
+# Plan
+
+# 0 - 9999 possible 2fa codes
+# n number of worker processes
+# 10,000 / n codes to check per worker
+# Ex) worker 1 does 0 - 999, worker 2 does 1000 - 1999 ... worker n does (n-1 * 1000) to (n*1000 - 1)
+
+# Each worker will get it's own csrf token w/ logging in, so don't need to worry about an overall 2 attempt counter.
+# So the process, per worker, will be: login, get csrf, try 2 codes (check each time if 2fa code is right), repeat.
+
+# Each call to the function that carries out one functional unit of the above process (2fa attempt) will
+# return a dict containing the status, and 2fa code, for that attempt. That way it will be easier to filter
+# out bad codes based on status in the end.
+
+# STARTING HERE WE NEED TO LOGIN EVERY TWO TIMES
+
+
+def try_2fa_functional_unit():
+    pass
+
+
 # Then: Grab response after successful login to grab the csrf token for interaction with 2fa login page
 #       Note: Only 2 2fa code guesses per login, so we will need to restart every two guess from here.
 response_text = login('carlos', 'montoya', csrf)
